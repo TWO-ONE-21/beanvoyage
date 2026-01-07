c75250db85eb47fb8fbb5df3ce2d1e8205b1120e
TWO-ONE-21 <ardorianda1809@gmail.com>
2026-01-07 09:32:06 +0800

    Hardcode kunci agar deploy berhasil

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-// --- CARA BARBAR (HARDCODE) ---
-// Langsung tempel nilainya di sini sebagai teks biasa.
-// Jangan lupa pakai tanda kutip dua ("...")
+// --- AREA TEMPEL KUNCI ---
 
+// 1. Masukkan URL Supabase Anda di antara tanda kutip di bawah:
 const supabaseUrl = "https://vyguofxpodgolidpehox.supabase.co"
+
+// 2. Masukkan Key Supabase (ANON KEY) Anda di antara tanda kutip di bawah:
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5Z3VvZnhwb2Rnb2xpZHBlaG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1MTU2MDEsImV4cCI6MjA4MjA5MTYwMX0.cEtUXtabt0V39ryRJ3mlMdxkcwrYUx-UvOvyqvrol8k"
+
+// -------------------------
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
